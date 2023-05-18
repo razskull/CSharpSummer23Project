d0208c649a7d9b6b6535f08128bae699d6376ef6
Richard Simpson <rjsimpson3@hotmail.com>
2023-05-18 00:34:40 -0400

    Inital Commit - Closes #1 and #2

---
+++ App.PracticePanther/App.PracticePanther/Models/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.PracticePanther.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+
+        public DateTime OpenDate { get; set; }
+
+        public DateTime CloseDate { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string Name { get; set; }
+
+        public string Notes { get; set; }
+
+        public Client() {
+            Name = string.Empty;
+        }
+    }
+}
---
+++ App.PracticePanther/App.PracticePanther/Models/Project.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.PracticePanther.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        public DateTime OpenDate { get; set; }
+
+        public DateTime CloseDate { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string ShortName { get; set; }
+
+        public string LongName { get; set; }
+
+        public string Name { get; set; }
+
+        public int ClientId { get; set; }
+
+    }
+}
